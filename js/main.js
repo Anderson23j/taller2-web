dbd71e2f7980f13b07ff50441a065a88af85c281
Anderson23j <andersonvalenciaj23@gmail.com>
2023-04-17 11:29:02 -0500

    a

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 let producto = document.getElementById("pro")
 let unidad = document.getElementById("uni")
 let valor = document.getElementById("val")
+let array = [producto,unidad,valor]
+
+let bt1 = document.getElementById("btn1")
+
+bt1.addEventListener('click', function(){
+    let dup = array.push ([producto.value,unidad.value,valor.value])
+    alert(dup)
+    
+})
